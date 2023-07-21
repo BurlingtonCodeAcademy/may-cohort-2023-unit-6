831bc1a6b59a084c8f8a5b7be2a043f40e861cc1
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-07-20 20:57:51 -0400

    bcrypt jwt notes

--- 01_Movies-MongoDB/controllers/user.controller.js
+++ 01_Movies-MongoDB/controllers/user.controller.js
@@ -1,8 +1,32 @@
 const router = require('express').Router();
 const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.JWT;
 
+// const testingBcrypt = (password) => {
+//     let encrypt = bcrypt.hashSync(password, 10);
+//     console.log('Encrypt:', encrypt);
+// }
+
+
+/*
+! CHALLENGE:
+    - Add a boilerplate code for the controller
+    - Create a POST method route ('/signup')
+    - Make sure route is working
+        - simple response of "Connected"
+        - Test in Postman
+    - full URL is:
+        - localhost:4000/user/signup
+*/
 router.post('/signup', async (req, res) => {
     // res.send('Connected!')
+
+    // testingBcrypt('myPassword');
+    // testingBcrypt('myPassword');
+    // testingBcrypt('myNewPassword');
+
     try {
         
         // Creating a new object based off the Model Schema
@@ -10,14 +34,19 @@ router.post('/signup', async (req, res) => {
             firstName: req.body.first,
             lastName: req.body.last,
             email: req.body.email,
-            password: req.body.password,
+            // password: req.body.password,
+            password: bcrypt.hashSync(req.body.password,13)
         });
 
         const newUser = await user.save(); // writes to database. Returns a response - this is why we need an "await"
 
+        // const token = jwt.sign({id: newUser._id}, "My Secret Mesage", {expiresIn: "1 day"});
+        const token = jwt.sign({id: newUser._id}, SECRET, {expiresIn: "1 day"});
+
         res.status(200).json({
             user: newUser,
-            message: 'Success!'
+            message: 'Success!',
+            token
         })
 
     } catch (err) {
@@ -26,5 +55,42 @@ router.post('/signup', async (req, res) => {
         })
     }
 });
+
+router.post('/login', async (req,res) => {
+    try {
+        
+        //*1. Capture data provided by user (body - req.body)
+        // const email = req.body.email
+        // const password = req.body.password
+        const { email, password } = req.body
+
+        //*2. Check database to see if email supplied exists.
+        const user = await User.findOne({email: email});
+        // A MongoDB method that accepts a query as an argument. Returns an instance of a document that matches.
+        // console.log(user);
+        
+        //*3. If email exists, consider if passwords match.
+        const passwordMatch = await bcrypt.compare(password, user.password); // returns a true/false value
+        // compare(string, hashed)
+        // console.log(passwordMatch)
+
+        if(!user || !passwordMatch) throw new Error('Email or Password does not match');
+
+        //*4. After verfied, provide a jwt (token).
+        const token = jwt.sign({id: user._id}, SECRET, {expiresIn: "1 day"});
+
+        //*5. Provide a response
+        res.status(200).json({
+            message: `Success!`,
+            user, 
+            token
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        })
+    }
+})
 
 module.exports = router;
--- notes.md
+++ notes.md
@@ -92,14 +92,52 @@ const UserSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', UserSchema);
 ```
+
+## Bcrypt
+- `npm i bcrypt`
+- dependency that handles encryption of data.
+  - Most commonly used for passwords (not limited)
+
+## Encryption
+- Plain text password are not secure when stored within the database.
+- Allows for another layer of security (both user and db)
+
+## Hashing
+- An algorithm to change plain text into various characters.
+  - practically impossible to turn hashed value back to original string. (not impossible)
+- Encryption prior to storing in DB.
+- No matter length of string, hash value is the same length.
+  - like strings will result in the same hashed value.
+  - **needs** `salting`
+
+## Salting
+- includes random strings within the plain text being hashed
+- Makes for unpredictability for the hashed value
+- We can denote the number of "salts"
+  - Typically 10-13 interations
+
+example code:
 ```js
-/*
-! CHALLENGE:
-    - Add a boilerplate code for the controller
-    - Create a POST method route ('/signup')
-    - Make sure route is working
-        - simple response of "Connected"
-        - Test in Postman
-    - full URL is:
-        - localhost:4000/user/signup
-*/
+bcrypt.hashSync("abc", 10);
+```
+- first param = password.
+- second param = number of times the password will be salted.
+
+## JWT
+- JSON Web Token
+- `npm i jsonwebtoken`
+- A way for our server to authenticate the user.
+
+```js
+const token = jwt.sign({id: user._id}, "secret message", {expiresIn: 60*60*24});
+```
+- `sign(payload, message, options)`
+  - 3 Arguments
+    - payload
+      - In this sample, we are using an object that details the user
+    - encrypt/decrypt message
+      - passed in as a string in the sample
+      - Typically stored within the `.env` 
+    - Options (expiration)
+      - represents seconds or a string time span
+        - ex: `"2 days"` or `"10h"`
